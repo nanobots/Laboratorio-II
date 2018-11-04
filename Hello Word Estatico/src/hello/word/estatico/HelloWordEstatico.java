ba7839d3816268667adafff58ae9051edbc34f46
unknown <ro.correo@gmail.com>
2018-11-04 19:57:05 -0300

    estatico

---
+++ Hello Word Estatico/src/hello/word/estatico/HelloWordEstatico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.word.estatico;
+
+/**
+ *
+ * @author note
+ */
+public class HelloWordEstatico {
+	static {
+        System.out.println("Hello world!");
+    }
+ 
+    public static void main(String... args) {
+ 
+    }
+}

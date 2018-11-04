6fb52e15a305170a8616e509d9a635925c5d3360
unknown <ro.correo@gmail.com>
2018-11-04 19:58:14 -0300

    instancia

---
+++ Hello Word Instancia/src/hello/world/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author note
+ */
+public class HelloWorld {
+
+    {
+        System.out.println("Hello world!");
+    }
+ 
+    public static void main(String... args) {
+        new HelloWorld();
+    }
+ }   
